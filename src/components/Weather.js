f838f138b4a422cdaad1b2bd9216e69712068b12
since9teen94 <droyanez@gmail.com>
2021-12-21 08:44:00 -0600

    more styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./Weather.module.css";
 const Weather = (props) => {
-  const APIKEY = process.env.REACT_APP_API_KEY
+  const APIKEY = process.env.REACT_APP_API_KEY;
   const lat = "40.71";
   const long = "74.01";
   const CALL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKEY}`;
@@ -33,14 +33,14 @@ const Weather = (props) => {
     <div className={styles.forecast}>
       {weather.map((day) => (
         <div key={day[0]} className={styles.day}>
-          <h1>{day[0]}</h1>
+          <h2>{day[0]}</h2>
           <img
             src={`http://openweathermap.org/img/wn/${day[3]}@2x.png`}
             alt="Weather"
           />
-          <div>
-            <p>Min: {day[1]}</p>
-            <p>Max: {day[2]}</p>
+          <div className={styles.minMax}>
+            <p>{Math.round(day[1])}&deg;</p>
+            <p>{Math.round(day[2])}&deg;</p>
           </div>
         </div>
       ))}
